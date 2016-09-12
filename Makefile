6e36bce18cfb96e8e60586a7fefb6ce90885a627
Mattias Lasersköld <mattias.l.sk@gmail.com>
2016-09-12 11:44:59 +0200

    Moved to the client at www.messenger.com
    It does what i wanted all along

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 
 
-LIBS=-lQt5WebKitWidgets -L/usr/lib/x86_64-linux-gnu -lQt5Quick -lQt5OpenGL -lQt5PrintSupport -lQt5WebKit -lQt5Qml -lQt5Location -lQt5Widgets -lQt5Network -lQt5Positioning -lQt5Sensors -lQt5Gui -lQt5Core -lGL -lpthread -static-libgcc -static-libstdc++
-CFLAGS=-std=c++11
-CFLAGS+=-I/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64 -I. -I/usr/include/qt5 -I/usr/include/qt5/QtWebKitWidgets -I/usr/include/qt5/QtQuick -I/usr/include/qt5/QtOpenGL -I/usr/include/qt5/QtPrintSupport -I/usr/include/qt5/QtWebKit -I/usr/include/qt5/QtQml -I/usr/include/qt5/QtLocation -I/usr/include/qt5/QtWidgets -I/usr/include/qt5/QtNetwork -I/usr/include/qt5/QtPositioning -I/usr/include/qt5/QtSensors -I/usr/include/qt5/QtGui -I/usr/include/qt5/QtCore -I.
+LIBS=-lQt5WebKitWidgets -L/usr/lib/x86_64-linux-gnu -lQt5Quick -lQt5OpenGL -lQt5PrintSupport -lQt5WebKit -lQt5Qml -lQt5Widgets -lQt5Network -lQt5Gui -lQt5Core -lGL -lpthread -static-libgcc -static-libstdc++
+CFLAGS=-std=c++11 -g
+CFLAGS+=`pkg-config --cflags Qt5WebKit Qt5Gui` -I.
 
 all:
 	g++ main.cpp -o facebook-chat  ${LIBS} ${CFLAGS}  -fPIC
--- main.cpp
+++ main.cpp
@@ -8,8 +8,40 @@
 QWebView *pubView;
 
 
-const QString code("document.getElementById('pagelet_bluebar').style.display = 'none'; \
-document.getElementsByClassName('ego_section')[0].style.display = 'none';");
+class PersistentCookieJar : public QNetworkCookieJar {
+public:
+    PersistentCookieJar(QObject *parent) : QNetworkCookieJar(parent) { load(); }
+    ~PersistentCookieJar() { save(); }
+
+public:
+    void save()
+    {
+        QList<QNetworkCookie> list = allCookies();
+        QByteArray data;
+        foreach (QNetworkCookie cookie, list) {
+            if (!cookie.isSessionCookie()) {
+                data.append(cookie.toRawForm());
+                data.append("\n");
+            }
+        }
+        QSettings settings;
+        settings.setValue("Cookies",data);
+    }
+
+    void load()
+    {
+        QSettings settings;
+        QByteArray data = settings.value("Cookies").toByteArray();
+        setAllCookies(QNetworkCookie::parseCookies(data));
+    }
+};
+
+const QString code(R"tag(
+
+document.getElementById('pagelet_bluebar').style.display = 'none';
+document.getElementsByClassName('ego_section')[0].style.display = 'none';
+
+)tag");
 
 //------------------Class to handle hiding of the blue bar------------
 
@@ -33,6 +65,8 @@ public:
 protected:
     void timerEvent(QTimerEvent *){
     	auto apa = runScript("document.readyState");
+    	auto title = runScript("document.title").toString();
+    	pubView->setWindowTitle(title);
     	if(apa.toString().compare("complete") == 0 ){
 
     		if (runScript("document.getElementById('stream_pagelet') != null") == "true") {
@@ -47,18 +81,26 @@ protected:
 private:
 };
 
+
+void setPersistance(QWebView *view) {
+    auto jar = new PersistentCookieJar(view);
+    view->page()->networkAccessManager()->setCookieJar(jar);  // the jar is reparented to the page
+    jar->setParent(view);  // reparent to main widget to avoid destruction together with the page
+}
+
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QWebView view;
 
     pubView = &view;
-
+    setPersistance(&view);
     view.show();
-    view.setUrl(QUrl("https://www.facebook.com/messages"));
+    view.setUrl(QUrl("https://www.messenger.com"));
 
 	timer qtim(&app);
 	qtim.start(1000);
     auto ret = app.exec();
+    qtim.stop();
     app.quit();
     return ret;
 }
